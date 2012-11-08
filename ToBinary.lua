f4aea3f36ca5cd10c22c3c66939a8e296a13162a
Elijah Frederickson <elijah.frederickson@gmail.com>
2012-11-07 16:53:35 -0900

    Initial commit

---
+++ ToBinary.lua
@@ -0,0 +1,61 @@
+-- Reverse bytes
+function reverse(t)
+  local nt = { } -- new table
+  local size = #t + 1
+  for k, v in ipairs(t) do
+    nt[size - k] = v
+  end
+  return nt
+end
+
+-- Encode bytes
+function tobits(num)
+    local t={ }
+    while num > 0 do
+        rest = num % 2
+        t[#t + 1] = rest
+        num = (num - rest) / 2
+    end
+    t = reverse(t)
+    return table.concat(t)
+end
+
+function encode(str)
+    local result = ""
+    for i = 1, str:len() do
+        result = result .. tobits(str:sub(i, i):byte()) .. " "
+    end
+    return result
+end
+
+local decodeSource = [[function decode(str)
+    local result = ""
+    local last = ""
+    for i = 1, str:len() do
+        local char = str:sub(i, i)
+        if char == " " then
+            if last ~= "" then
+                result = result .. string.char(tonumber(last, 2))
+                last = ""
+            end
+        else
+            last = last .. char
+        end
+    end
+    return result
+end]]
+loadstring(decodeSource)() -- create decode function
+
+function encode2(str)
+    return decodeSource .. "\r\nlocal encoded = [[" .. str .. "]]\r\nloadstring(decode(encoded))()"
+end
+
+local code = [[local str = "Hello world!"
+print(str)]] -- could be game.Workspace["A script to obfuscate..."].Source
+local enc = encode2(encode(code))
+--local dec = decode(enc)
+--print("Encoded: ", enc)
+--print("Decoded: ", dec)
+print("Output: ")
+--loadstring(dec)()
+loadstring(enc)()
